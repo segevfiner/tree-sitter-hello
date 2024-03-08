f9418340b60246a616881ecbbe62922ff22bcd35
Segev Finer <segev@swimm.io>
2024-03-09 01:07:19 +0200

    Add prebuildify

--- bindings/node/index.js
+++ bindings/node/index.js
@@ -1,18 +1,6 @@
-try {
-  module.exports = require("../../build/Release/tree_sitter_hello_binding");
-} catch (error1) {
-  if (error1.code !== "MODULE_NOT_FOUND") {
-    throw error1;
-  }
-  try {
-    module.exports = require("../../build/Debug/tree_sitter_hello_binding");
-  } catch (error2) {
-    if (error2.code !== "MODULE_NOT_FOUND") {
-      throw error2;
-    }
-    throw error1
-  }
-}
+const path = require("path");
+
+module.exports = require("node-gyp-build")(path.join(__dirname, "../.."));
 
 try {
   module.exports.nodeTypeInfo = require("../../src/node-types.json");
